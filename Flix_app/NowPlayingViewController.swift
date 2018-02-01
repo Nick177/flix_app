93364f85682cc7aeae137a8ebc4474b1457f8284
Nicholas Rosas <nrosas@csumb.edu>
2018-01-31 18:05:59 -0800

    Add pull to refresh

--- Flix_app/NowPlayingViewController.swift
+++ Flix_app/NowPlayingViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
+        fetchMovies()
         
+    }
+    
+    func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -35,10 +48,10 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         task.resume()
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -55,6 +68,12 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseUrlString = "https://image.tmdb.org/t/p/w500"
+        
+        let posterUrl = URL(string: baseUrlString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterUrl)
         
         return cell
     }
