a27b762aee0167a6e6dca8a77fd6f9d0222f436b
Nicholas Rosas <nrosas@csumb.edu>
2018-02-06 17:47:28 -0800

    Add collection view grid

---
+++ Flix_app/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_app
+//
+//  Created by Nicholas Rosas on 2/5/18.
+//  Copyright © 2018 Nicholas Rosas. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
---
+++ Flix_app/SuperheroViewController.swift
@@ -0,0 +1,92 @@
+//
+//  SuperheroViewController.swift
+//  Flix_app
+//
+//  Created by Nicholas Rosas on 2/5/18.
+//  Copyright © 2018 Nicholas Rosas. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellsPerRow: CGFloat = 2
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerRow - 1)
+        let width = collectionView.frame.size.width / cellsPerRow - interItemSpacingTotal / cellsPerRow
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        
+        fetchMovies()
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let movie = movies[indexPath.item]
+        if let posterPathstring = movie["poster_path"] as? String {
+            let baseURLString = "https://image.tmdb.org./t/p/w500"
+            let posterURL = URL(string: baseURLString + posterPathstring)!
+            
+            cell.posterImageView.af_setImage(withURL: posterURL)
+        }
+        
+        return cell
+    }
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request)
+        { (data, response, error) in
+            // this will run when network request returns
+            if let error = error { // check if nil
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+                //self.refreshControl.endRefreshing()
+                //self.activityIndicator.stopAnimating()
+            }
+        }
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
